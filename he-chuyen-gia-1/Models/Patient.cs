1fde22b36bf837656eb920cc76f92aedb2cbda87
XuanEthan <phgxuanhuy@gmail.com>
2025-09-16 22:05:15 +0700

    sua-entity-patient : them key chinh

--- he-chuyen-gia-1/Models/Patient.cs
+++ he-chuyen-gia-1/Models/Patient.cs
@@ -5,10 +5,11 @@ namespace hechuyengia.Models
 {
     public class Patient
     {
+        [Key]
         public int PatientId { get; set; }
-        [Required, StringLength(100)] public string FullName { get; set; } = "";
+        [Required, StringLength(100)] public string FullName { get; set; } = string.Empty;
         [DataType(DataType.Date)] public DateTime Dob { get; set; }
-        [Required] public string Gender { get; set; } = "Nam";
+        [Required] public string Gender { get; set; } = string.Empty;
         public ICollection<Diagnosis> Diagnoses { get; set; } = new List<Diagnosis>();
     }
 }
