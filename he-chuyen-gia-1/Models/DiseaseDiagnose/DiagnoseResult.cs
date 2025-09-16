9c0d575aced475dfcda3ded73c374ddc8059d9f6
XuanEthan <phgxuanhuy@gmail.com>
2025-09-16 22:03:43 +0700

    sua-entity : them required

--- he-chuyen-gia-1/Models/DiseaseDiagnose/DiagnoseResult.cs
+++ he-chuyen-gia-1/Models/DiseaseDiagnose/DiagnoseResult.cs
@@ -9,18 +9,22 @@ namespace hechuyengia.Models.DiseaseDiagnose
         [Key]
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage ="Mã BN đang để trống !")]
         public int PatientId { get; set; }
+
         public Patient? Patient { get; set; }
 
+        [Required(ErrorMessage = "Kết luận triệu chứng đang để trống !")]
         public string Symptoms { get; set; } = string.Empty;
+        [Required(ErrorMessage ="Kết luận bệnh đang để trống !")]
         public string Diseases { get; set; } = string.Empty;
+        [Required(ErrorMessage ="Đề xuất thuốc đang để trống ! ")]
         public string MedicinesAdvice { get; set; } = string.Empty;
         public string DoctorAdvice { get; set; } = string.Empty;
 
+        [Required]
         [Column(TypeName = "datetime")]
         public DateTime DiagnoseDate { get; set; }
-
         public string DoctorName { get; set; } = string.Empty;
     }
 }
